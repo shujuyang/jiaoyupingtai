51dc35f79508194f85f168a500139a31eb04eda4
yangshujie <292679979@qq.com>
2017-08-28 22:28:13 +0800

    下载了webpack 打包时需要的工具，并配置的其配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
 var mypath = require('path')
+var htmlPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './vue/app.js',
     output: {
         path: mypath.join(__dirname,'./dist'),
         filename: 'hebing.js'
     },
+    plugins: [
+        new htmlPlugin({
+            template: './index.html' , // 指定复制哪个html
+            filename: 'index.html' // 指定复制后的html叫什么名字
+        })
+    ],
     module: {
         // 不知是可以打包 css，还可以打包less
         // 也可以处理sass，或者 *.vue ，图片，或者字体
         rules: [
-            test: /\.css$/,
-            loader: ['style-loader','css-loader']
+            {
+                test: /\.css$/, // 匹配css文件
+                loader: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(ttf|woof|woof2|svg|eot)$/,  // 匹配各种后缀的字体文件
+                loader:['file-loader?name=./fonts/[name].[ext]']
+            },
+            {
+                test: /\.(jpg|jpeg|png|bmp|gif|svg)$/,  // 匹配各种格式的图片
+                loader: ['url-loader']
+            },
+            {
+                test: /\.vue$/,
+                loader: ['vue-loader']
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader', // 需要下载 babel-loader这个包和babel-preset-es2015这个包
+                exclude: /node_modules/ // 忽略node_modules文件夹，不要去将node_modules中的js转换为es5
+            }
         ]
     }
 }
